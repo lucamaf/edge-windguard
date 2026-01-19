57a3a3fefa9880ab50bad2d87c9bdf461110c92e
lucamaf <luferrar@redhat.com>
2026-01-19 07:56:00 +0100

    adding deployment for inference

--- inference_svc/predictor.py
+++ inference_svc/predictor.py
@@ -78,4 +78,4 @@ def get_prediction():
 
 if __name__ == '__main__':
     # host='0.0.0.0' makes it accessible from other containers/machines
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=8080)
